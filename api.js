949f83d7d1e1d33668c1632a947410487d25f374
stepkobetsu-hub <stepkobetsu@gmail.com>
2026-07-03 06:03:15 +0900

    Add files via upload

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbxIH2VtgwRi50xduXgrkYrjD0yrzNfQ5vCWt1XgOzil6LZSgXNj6MJo9jPYvOkjNHdu/exec';
+
 function jsonp(action, params = {}) {
   return new Promise((resolve, reject) => {
     const callbackName = 'cb_' + Date.now() + '_' + Math.random().toString(36).slice(2);
@@ -36,5 +37,6 @@ const api = {
   archiveHistory: (id) => postJson({ action: 'archiveHistory', id }),
   saveTemplate: (payload) => postJson({ action: 'saveTemplate', ...payload }),
   saveTemplateAs: (payload) => postJson({ action: 'saveTemplateAs', ...payload }),
-  deleteTemplate: (id) => postJson({ action: 'deleteTemplate', id })
+  deleteTemplate: (id) => postJson({ action: 'deleteTemplate', id }),
+  refreshStudents: () => postJson({ action: 'refreshStudents' })
 };
